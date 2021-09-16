f42a1a6d9053e9194bd533d64a937eb043f32689
serialLinker <cyril.wirth@gmail.com>
2021-09-16 12:58:03 +0200

    Add files via upload

---
+++ script/data.js
@@ -0,0 +1,53 @@
+translations = [
+    {
+        "french" : "Chaise",
+        "korean": "의자"
+    },
+    {
+        "french" : "Lunettes",
+        "korean": "안경"
+    },
+    {
+        "french" : "Plantes",
+        "korean": "화분"
+    },
+    {
+        "french" : "Fenêtre",
+        "korean": "창문"
+    },
+    {
+        "french" : "Lit",
+        "korean": "침대"
+    },
+    {
+        "french" : "Maison",
+        "korean": "집"
+    },
+    {
+        "french" : "Porte",
+        "korean": "문"
+    },
+    {
+        "french" : "Baguettes",
+        "korean": "젓가락"
+    },
+    {
+        "french" : "Cuillère",
+        "korean": "숫가락"
+    }
+];
+
+configurations = [
+    {
+        "from" : "french",
+        "fromLabel" : "Français",
+        "to": "korean",
+        "toLabel" : "한국어"
+    },
+    {
+        "from" : "korean",
+        "fromLabel" : "한국어",
+        "to": "french",
+        "toLabel" : "Français"
+    },
+]
